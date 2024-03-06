beb51c8f32f3f405da73eaab00b074bf8d59b8fb
Srin1234 <161831147+Srin1234@users.noreply.github.com>
2024-03-06 12:55:18 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -31,17 +31,17 @@ variable "location_name" {
 
 variable "resource_group_name" {
   description = "Azure Resource Group name"
-  default = "smithaciactsrg"
+  default = "sriniciactsrg"
 }
 
 variable "mysql_server_name" {
   description = "Azure MySQL Server name"
-  default = "smithaciactsmysql"
+  default = "sriniciactsmysql"
 }
 
 variable "mysql_server_login" {
   description = "Azure MySQL Server Login User"
-  default = "smithaciactsuser"
+  default = "sriniciactsuser"
 }
 
 variable "mysql_server_password" {
